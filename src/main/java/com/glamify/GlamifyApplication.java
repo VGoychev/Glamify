9139ea447827275c73c0d338efa7f2cab776868e
VGoychev <vladimirgoichev@gmail.com>
2025-07-01 22:03:44 +0300

    Initial commit

---
+++ src/main/java/com/glamify/GlamifyApplication.java
@@ -0,0 +1,13 @@
+package com.glamify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlamifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlamifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glamify/GlamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlamifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
